99e4db3a2b2b7d86bc29c6f56d9a25cf08c0e355
Jdwitts15a <james.witts1@nhm.ac.uk>
2025-01-23 14:29:37 +0000

    Added something to make a graph

--- test-script.R
+++ test-script.R
@@ -7,3 +7,18 @@
 #hello!!!
 #This is the 3rd script I've done
 #And I keep updating it.
+
+# srisarya code :)
+srishti_data<-data.frame(x=1:10,
+                         y=1:10)
+
+ggplot(srishti_data, aes(x = x, y = y))+
+  geom_point(colour="darkcyan")+
+  theme_bw
+
+# jdwitts15a code :)
+jw_data<-data.frame(x=1:10, y=1:10)
+
+ggplot(mydata, aes(x = x, y = y))+
+  geom_point(colour="deeppink")+
+  theme_bw
